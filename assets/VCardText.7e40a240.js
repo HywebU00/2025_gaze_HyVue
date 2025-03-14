ccfa5eb8a7644eaed78556c195ea0497b855d0c9
margo-lee <margo.lee@hyweb.com.tw>
2025-03-14 17:30:09 +0800

    deploy

---
+++ assets/VCardText.7e40a240.js
@@ -0,0 +1,1 @@
+import{as as t}from"./index.f3b302f9.js";const e=t("v-card-text");export{e as V};
--- assets/VCardText.8a842f6e.js
+++
@@ -1,1 +0,0 @@
-import{c as t}from"./createSimpleFunctional.47eb7c01.js";const e=t("v-card-text");export{e as V};
