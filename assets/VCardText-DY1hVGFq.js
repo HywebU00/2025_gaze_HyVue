1578e1e6006156c48aa1ffcdcbfebd0216139103
margo-lee <margo.lee@hyweb.com.tw>
2025-04-01 17:43:25 +0800

    deploy

--- assets/VCardText-DY1hVGFq.js
+++
@@ -1,1 +0,0 @@
-import{u as r,a as s,m as o}from"./index-CCD4FGa3.js";import{v as m,p as c,m as n}from"./index-Du4x6ymM.js";const p=c({opacity:[Number,String],...o(),...s()},"VCardText"),x=m()({name:"VCardText",props:p(),setup(t,a){let{slots:e}=a;return r(()=>n(t.tag,{class:["v-card-text",t.class],style:[{"--v-card-text-opacity":t.opacity},t.style]},e)),{}}});export{x as V};
